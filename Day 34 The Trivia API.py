76ea3b249fb995714722bc347d0cb80ae03788db
Phensic <juliusayoola5@gmail.com>
2022-02-11 03:07:29 +0100

    --- Auto-Git Commit ---

--- Day 34 The Trivia API.py
+++ Day 34 The Trivia API.py
@@ -1,5 +1,17 @@
-# THE TRIVIA API AND THE QUIZLER APP
+# THE TRIVIA API AND THE QUIZZLER APP
 import requests
+import html 
+from  tkinter import *
+
+
+class QuizInterface:
+    
+    def __init__(self, window):
+        self.window = Tk()
+
+        self.windoe
+        self.window.mainloop()
+
 
 parameters = {
     "amount": 10,
@@ -15,87 +27,69 @@ question_data = data["results"]
 
 
 
-
-
-
-
-
-
-
-# # TRUTH OR FALSE GAME
-
-# question_data = [
-#     {"text": "A slug's blood is green.", "answer": "True"},
-#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-#     {
-#         "text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-#                 "to eat.",
-#         "answer": "True"},
-#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-#      "answer": "False"},
-#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-# ]
+# TRUTH OR FALSE GAME
 
 
 # TODO 1. CREATE A QUESTION CLASS FROM SCRATCH
-# class Question:
+class Question:
 
-#     def __init__(self, text, answer):
-#         self.text = text
-#         self.answer = answer
-
-
-
-# Question_bank = []
-# for questions in question_data:
-#     question_text = questions["text"]
-#     question_answer = questions["answer"]
-#     new_question = Question(question_text, question_answer)
-#     Question_bank.append(new_question)
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
 
 
-# class QuizBrain:
 
-#     def __init__(self, q_list):
-#         self.question_number = 0
-#         self.score = 0
-#         self.question_list = q_list
-
-#     def next_question(self):
-#         current_question = self.question_list[self.question_number]
-#         self.question_number += 1
-#         user_answer = input(f"Q{self.question_number}: {current_question.text} (True/False): ")
-#         self.check_answer(user_answer, current_question.answer)
-
-#     def still_has_question(self):
-#         return self.question_number < len(self.question_list)
-
-#     def check_answer(self, user_answer, correct_answer):
-#         if user_answer.lower() == correct_answer.lower():
-#             self.score += 1
-#             print("You got it right!")
-#         else:
-#             print("That's wrong.")
-#         print(f"The correct answer is {correct_answer}.")
-#         print(f"Your current score is: {self.score}/{self.question_number}")
-#         print("\n")
+Question_bank = []
+for questions in question_data:
+    question_text = questions["question"]
+    question_answer = questions["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    Question_bank.append(new_question)
 
 
-# quiz = QuizBrain(Question_bank)
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        question_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q{self.question_number}: {question_text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
+
+quiz = QuizBrain(Question_bank)
+quiz_ui = QuizInterface()
+
+
 
 # while quiz.still_has_question():
 #     quiz.next_question()
 
-# print("You've completed the quiz")
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
 
+
+# GUI INTERFACE
+
+THEME_COLOR = "#375362"
 
 
 
